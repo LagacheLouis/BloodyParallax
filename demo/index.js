4593d89882acd07072874536832bcd5891d9b95b
Louis Lagache <louislagache@gmail.com>
2022-05-05 21:31:29 -0500

    node module

--- demo/index.js
+++ demo/index.js
@@ -2,8 +2,9 @@ import BloodyParallax from "../index.js";
 
 
 window.onload = () => {
+    const bubbles = document.getElementById("bubbles");
     //SPHERES GENERATION
-    for(let i = 0; i < 300; i++){
+    for(let i = 0; i < 100; i++){
         const wrapper = document.createElement("div");
         wrapper.classList.add("bubble");
         const inner = document.createElement("div");
@@ -11,11 +12,11 @@ window.onload = () => {
 
         const zIndex = Math.random();
         const size = Math.random() * 1.7 + 0.1;
-        const y = document.body.clientHeight * 2 * Math.random() - document.body.clientHeight/2 - 50;
-        const x = window.innerWidth * 2 * Math.random() - window.innerWidth/2 - 50;
+        const y = Math.random() * 100;
+        const x = Math.random() * 100;
 
-        wrapper.style.top = `${y}px`;
-        wrapper.style.left = `${x}px`;
+        wrapper.style.top = `${y}%`;
+        wrapper.style.left = `${x}%`;
         wrapper.style.zIndex = `${Math.floor(zIndex * 100)}`;
         
         inner.style.background = `radial-gradient(rgba(255,0,43,${zIndex}), black 50px) no-repeat 10px -10px, black`;
@@ -23,10 +24,10 @@ window.onload = () => {
 
 
         wrapper.setAttribute("data-depth",zIndex);
-        document.body.append(wrapper);
+        bubbles.append(wrapper);
     }
     setTimeout(()=>{
-        const parallax = new BloodyParallax({mouseIntensity: 0, scrollIntensity: 0.1, damping: 0.1});
+        const parallax = new BloodyParallax({mouseIntensity: 0.1, scrollIntensity: 0.2, damping: 0.1});
     },10);
 
 
